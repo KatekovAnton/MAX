865125232fa57a7ea59d7d8e120cc6897b592b81
AntonWork <Admin@.(none)>
2011-10-13 13:13:06 +0400

    UIWindowManager

---
+++ MAXNew/MAXNew/UI/UIWindowManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MAXNew.UI
+{
+    public class UIWindowManager
+    {
+        List<UIControlController> _windowsQueue;
+        List<UIControlController> _showingWindowsList;
+        UIControl _rootScene;
+/*
+        + (UIWindowsManager *)instance;
+
+- (void)discardWindow:(UIViewController *)window;
+- (void)queueWindow:(UIViewController *)window;
+- (void)popupWindow:(UIViewController *)window;
+- (void)addWindow:(UIViewController *)window popup:(BOOL)popup;*/
+    }
+}
